f5b00cfa7241467cd679f9424da80bd37f4883b1
Aviatore <wojtek.wes@gmail.com>
2020-06-24 13:39:33 +0200

    Implement check_all_ships_are_placed() function
    Additional modifications:
    - correct the bug in the shot() function;
    - add clear() and print_table() functions in the place_ship_loop() function;
    - comment-out the debug loop in the main() function.

--- battleship.py
+++ battleship.py
@@ -89,11 +89,11 @@ def shot(opponent_board, oponent_ship_stats, player, row, col):
                 ship['shot'].append([row, col])
                 if ship['len'] == len(ship['shot']):
                     for cord in ship['shot']:
-                        opponent_board[cord[0], cord[1]] = 'S'
+                        opponent_board[cord[0]][cord[1]] = 'S'
                 else:
-                    opponent_board[row, col] = 'H'
+                    opponent_board[row][col] = 'H'
     else:
-        opponent_board[row, col] = 'M'
+        opponent_board[row][col] = 'M'
 
 
 def is_all_ships_destroyed(board, ship_stats):
@@ -290,14 +290,20 @@ def check_all_ships_are_placed(ships):
     """Checks the remaining number of ships to be placed.
        If the number is non-zero, the function returns True.
        If there are no ships left, it returns False."""
-    pass
+    for ship_type in ships:
+        if ships[ship_type][1] > 0:
+            return True
+    else:
+        return False
 
 
 def place_ship_loop(board, player, ship_stats, ships):
     __ships = copy.deepcopy(ships)
     
     while check_all_ships_are_placed(__ships):
+        clear()
         print_board(board)
+        print_table(__ships, 5, 24)
         place_ship(board, player, ship_stats, __ships)
 
 
@@ -389,12 +395,12 @@ def main():
     #         },
     # }
 
-    while True:
-        clear()
-        print_board(board1)
-        print_table(ships, 5, 24)
-        place_ship(board1, player1, ship_stats1, ships)
-    exit()
+    # while True:
+    #     clear()
+    #     print_board(board1)
+    #     print_table(ships, 5, 24)
+    #     place_ship(board1, player1, ship_stats1, ships)
+    # exit()
 
     
 
